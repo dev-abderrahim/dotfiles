d03b5ddd5038c2245030e060d534713f1b4aa055
Abderrahim G. <rahimguerrouf0@gmail.com>
2026-05-29 09:53:24 +0100

    Neovim: Changed colorscheme

---
+++ dot_config/nvim/lua/plugins/colorschemes.lua
@@ -0,0 +1,12 @@
+return {
+    {
+        "miikanissi/modus-themes.nvim",
+        priority = 1000 ,
+        opt = {
+            variants = {
+		        modus_operandi = "deuteranopia",
+		        modus_vivendi = "deuteranopia"
+        	},
+        },
+    }
+}
